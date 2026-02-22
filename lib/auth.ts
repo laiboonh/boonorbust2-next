48057137fd6cfe83f309c03e486419eb955fe11b
Boon <laiboonh@gmail.com>
2026-02-22 12:50:08 +0800

    Turn on debug mode

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,6 +4,7 @@ import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  debug: true,
   adapter: PrismaAdapter(prisma),
   providers: [
     Google({
